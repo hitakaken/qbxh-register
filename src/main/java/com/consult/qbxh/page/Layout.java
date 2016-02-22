3f2adbbbbb2cd5279fe63480227825855b5efd46
kcao <kcao@libnet.sh.cn>
2016-02-22 11:49:47 +0800

    IE8 Support

--- src/main/java/com/consult/qbxh/page/Layout.java
+++ src/main/java/com/consult/qbxh/page/Layout.java
@@ -20,6 +20,14 @@ public abstract class Layout {
 
     public abstract String getPageTitle();
 
+    public String getIe8_support(){
+        return "<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->\n" +
+                "    <!--[if lt IE 9]>\n" +
+                "    <script src=\"/qbxh/assets/bootswatch/bower_components/html5shiv/dist/html5shiv.js\"></script>\n" +
+                "    <script src=\"/qbxh/assets/bootswatch/bower_components/respond/dest/respond.min.js\"></script>\n" +
+                "    <![endif]-->";
+    }
+
     protected void redirect(String url) throws IOException {
         response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
         response.sendRedirect(request.getContextPath() + url);
